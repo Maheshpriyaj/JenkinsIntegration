8d6a410ddcc3f3100102897e8d134a7d65452d66
Maheshpriya <maheshpriyamca19@gmail.com>
2024-09-13 22:32:44 +0530

    added

--- src/test/java/TestRunner/CucumberTestRunner.java
+++ src/test/java/TestRunner/CucumberTestRunner.java
@@ -8,10 +8,8 @@ import reports.GenerateCucumberReport;
   
   @CucumberOptions
   (tags="", features= {"src/test/resources/Features"}, glue=
-  {"StepDefinitions"}, plugin = {
-	        "pretty",
+  {"StepDefinitions"},plugin = {
 	        "json:target/cucumber-reports/Cucumber.json",
-	        "junit:target/cucumber-reports/Cucumber.xml",
 	        "html:target/cucumber-reports/cucumber-html-report.html"
 	    })
   
