0ebb2842fabaca7d901f822ec5fe6c7da509b3f5
Maheshpriya <maheshpriyamca19@gmail.com>
2024-09-13 22:15:20 +0530

    JVM Report

--- src/test/java/TestRunner/CucumberTestRunner.java
+++ src/test/java/TestRunner/CucumberTestRunner.java
@@ -1,11 +1,24 @@
   package TestRunner;
-  import io.cucumber.testng.AbstractTestNGCucumberTests; 
+  import org.testng.annotations.AfterSuite;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests; 
   import io.cucumber.testng.CucumberOptions;
+import reports.GenerateCucumberReport;
+
   
-  @CucumberOptions(tags="", features= {"src/test/resources/Features"}, glue=
-  {"StepDefinitions"}, plugin= {"pretty","html:target/htmlreport.html"})
+  @CucumberOptions
+  (tags="", features= {"src/test/resources/Features"}, glue=
+  {"StepDefinitions"}, plugin = {
+	        "pretty",
+	        "json:target/cucumber-reports/Cucumber.json",
+	        "junit:target/cucumber-reports/Cucumber.xml",
+	        "html:target/cucumber-reports/cucumber-html-report.html"
+	    })
   
   public class CucumberTestRunner extends AbstractTestNGCucumberTests {
+	  @AfterSuite
+	    public void generateReport() {
+		  GenerateCucumberReport.generateJVMReport("target/cucumber-reports/Cucumber.json");
   
-  }
+  }}
  
